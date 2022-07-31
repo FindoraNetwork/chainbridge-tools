36514af06f5461cc0e7d9a83bbed00541a694278
Charlie Chen <34498985+ws4charlie@users.noreply.github.com>
2022-07-31 15:37:57 -0500

    Erc20 pauser (#7)
    * exposed erc20 pauser-related commands
    
    * added pauser, minter, admin commands for erc721
    
    * added remove-admin into erc20 command

--- cb-sol-cli/cmd/erc721.js
+++ cb-sol-cli/cmd/erc721.js
@@ -4,6 +4,41 @@ const constants = require('../constants');
 const {Command} = require('commander');
 const {setupParentArgs, waitForTx, log} = require("./utils")
 
+const isAdminCmd = new Command("is-admin")
+    .description("Check if address is admin")
+    .option('--admin <value>', 'Address to check', constants.deployerAddress)
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        let res = await erc721Instance.hasRole(constants.ADMIN_ROLE, args.admin)
+        console.log(`[${args._name}] Address ${args.admin} ${res ? "is" : "is not"} an admin.`)
+    })
+
+const addAdminCmd = new Command("add-admin")
+    .description("Add an admin")
+    .option('--admin <address>', 'Address of admin', constants.adminAddresses[0])
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        log(args, `Adding ${args.admin} as an admin.`)
+        let tx = await erc721Instance.grantRole(constants.ADMIN_ROLE, args.admin)
+        await waitForTx(args.provider, tx.hash)
+    })
+
+const renounceAdminCmd = new Command("renounce-admin")
+    .description("Renounce an admin")
+    .option('--admin <address>', 'Address of current admin', constants.adminAddresses[0])
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .action(async function (args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        log(args, `Renouncing ${args.admin} as an admin.`)
+        let tx = await erc721Instance.renounceRole(constants.ADMIN_ROLE, args.admin)
+        await waitForTx(args.provider, tx.hash)
+    })
+
 const mintCmd = new Command("mint")
     .description("Mint tokens")
     .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
@@ -29,6 +64,18 @@ const ownerCmd = new Command("owner")
         log(args, `Owner of token ${args.id} is ${owner}`)
     })
 
+const isMinterCmd = new Command("is-minter")
+    .description("Check if address is minter")
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .option('--minter <value>', 'Address to check', constants.relayerAddresses[0])
+    .action(async function (args) {
+            await setupParentArgs(args, args.parent.parent)
+            const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+            let MINTER_ROLE = await erc721Instance.MINTER_ROLE();
+            let res = await erc721Instance.hasRole(MINTER_ROLE, args.minter)
+            console.log(`[${args._name}] Address ${args.minter} ${res ? "is" : "is not"} a minter.`)
+    })
+
 const addMinterCmd = new Command("add-minter")
     .description("Add a new minter to the contract")
     .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
@@ -39,6 +86,57 @@ const addMinterCmd = new Command("add-minter")
         const MINTER_ROLE = await erc721Instance.MINTER_ROLE()
         log(args, `Adding ${args.minter} as a minter of ${args.erc721Address}`)
         const tx = await erc721Instance.grantRole(MINTER_ROLE, args.minter);
+        await waitForTx(args.provider, tx.hash)
+    })
+
+    const removeMinterCmd = new Command("remove-minter")
+    .description("Remove a minter from the contract")
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .option('--minter <address>', 'Minter address', constants.relayerAddresses[1])
+    .action(async function(args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        let MINTER_ROLE = await erc721Instance.MINTER_ROLE();
+        log(args, `Removing ${args.minter} as a minter on contract ${args.erc721Address}`);
+        const tx = await erc721Instance.revokeRole(MINTER_ROLE, args.minter);
+        await waitForTx(args.provider, tx.hash)
+    })
+
+const isPauserCmd = new Command("is-pauser")
+    .description("Check if address is pauser")
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .option('--pauser <value>', 'Address to check', constants.relayerAddresses[0])
+    .action(async function (args) {
+            await setupParentArgs(args, args.parent.parent)
+            const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+            let PAUSER_ROLE = await erc721Instance.PAUSER_ROLE();
+            let res = await erc721Instance.hasRole(PAUSER_ROLE, args.pauser)
+            console.log(`[${args._name}] Address ${args.pauser} ${res ? "is" : "is not"} a pauser.`)
+    })
+
+const addPauserCmd = new Command("add-pauser")
+    .description("Add a new pauser to the contract")
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .option('--pauser <address>', 'Pauser address', constants.relayerAddresses[1])
+    .action(async function(args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        let PAUSER_ROLE = await erc721Instance.PAUSER_ROLE();
+        log(args, `Adding ${args.pauser} as a pauser on contract ${args.erc721Address}`);
+        const tx = await erc721Instance.grantRole(PAUSER_ROLE, args.pauser);
+        await waitForTx(args.provider, tx.hash)
+    })
+
+const removePauserCmd = new Command("remove-pauser")
+    .description("Remove a pauser from the contract")
+    .option('--erc721Address <address>', 'ERC721 contract address', constants.ERC721_ADDRESS)
+    .option('--pauser <address>', 'Pauser address', constants.relayerAddresses[1])
+    .action(async function(args) {
+        await setupParentArgs(args, args.parent.parent)
+        const erc721Instance = new ethers.Contract(args.erc721Address, constants.ContractABIs.Erc721Mintable.abi, args.wallet);
+        let PAUSER_ROLE = await erc721Instance.PAUSER_ROLE();
+        log(args, `Removing ${args.pauser} as a pauser on contract ${args.erc721Address}`);
+        const tx = await erc721Instance.revokeRole(PAUSER_ROLE, args.pauser);
         await waitForTx(args.provider, tx.hash)
     })
 
@@ -126,9 +224,17 @@ const proposalDataHashCmd = new Command("data-hash")
 
 const erc721Cmd = new Command("erc721")
 
+erc721Cmd.addCommand(isAdminCmd)
+erc721Cmd.addCommand(addAdminCmd)
+erc721Cmd.addCommand(renounceAdminCmd)
 erc721Cmd.addCommand(mintCmd)
 erc721Cmd.addCommand(ownerCmd)
+erc721Cmd.addCommand(isMinterCmd)
 erc721Cmd.addCommand(addMinterCmd)
+erc721Cmd.addCommand(removeMinterCmd)
+erc721Cmd.addCommand(isPauserCmd)
+erc721Cmd.addCommand(addPauserCmd)
+erc721Cmd.addCommand(removePauserCmd)
 erc721Cmd.addCommand(approveCmd)
 erc721Cmd.addCommand(depositCmd)
 erc721Cmd.addCommand(proposalDataHashCmd)
